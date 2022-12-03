bb59d4a92e09edea1706d5b3391fa841f7c7e823
Rajat <rajatky3107@gmail.com>
2022-12-03 18:18:55 +0530

    lsit, tuples and heaps

---
+++ start/maxHeap.go
@@ -0,0 +1,97 @@
+package main
+
+import "fmt"
+
+// MaxHeap struct has a slice that holds the array
+type MaxHeap struct {
+	array []int
+}
+
+// Insert adds an element to the heap
+func (h *MaxHeap) Insert(key int) {
+	h.array = append(h.array, key)
+	h.maxHeapifyUp(len(h.array) - 1)
+}
+
+// Extract returns the largest key, and removes it from heap
+func (h *MaxHeap) Extract() int {
+	extracted := h.array[0]
+	l := len(h.array) - 1
+
+	if len(h.array) == 0 {
+		fmt.Println("Cannot extract because array lenth is 0")
+		return -1
+	}
+	h.array[0] = h.array[l]
+	h.array = h.array[:l]
+
+	h.maxHeapifyDown(0)
+	return extracted
+}
+
+// maxHeapifyUp process
+func (h *MaxHeap) maxHeapifyUp(index int) {
+	for h.array[parent(index)] < h.array[index] {
+		h.swap(parent(index), index)
+		index = parent(index)
+	}
+}
+
+// maxHeapifyDown process
+func (h *MaxHeap) maxHeapifyDown(index int) {
+	lastIndex := len(h.array) - 1
+	l, r := left(index), right(index)
+	childToCompare := 0
+	// loop while index has at least one child
+	for l <= lastIndex { // when left child is the only child
+		if l == lastIndex {
+			childToCompare = l
+		} else if h.array[l] > h.array[r] { // when left child is larger
+			childToCompare = l
+		} else { // when right child is larger
+			childToCompare = r
+		}
+		// Compare array value of the current index to larger child and swap if smaller
+		if h.array[index] < h.array[childToCompare] {
+			h.swap((index), childToCompare)
+			index = childToCompare
+			l, r = left(index), right(index)
+		} else {
+			return
+		}
+	}
+}
+
+// Get parent index
+func parent(i int) int {
+	return (i - 1) / 2
+}
+
+// Get the left child index
+func left(i int) int {
+	return 2*i + 1
+}
+
+// Get the right child index
+func right(i int) int {
+	return 2*i + 2
+}
+
+// Swap keys in the array
+func (h *MaxHeap) swap(i1, i2 int) {
+	h.array[i1], h.array[i2] = h.array[i2], h.array[i1]
+}
+
+func main() {
+	m := &MaxHeap{}
+	buildHeap := []int{10, 20, 30, 5, 45, 87, 78, 12, 55}
+	for _, v := range buildHeap {
+		m.Insert(v)
+		fmt.Println(m)
+	}
+
+	for i := 0; i < 5; i++ {
+		m.Extract()
+		fmt.Println(m)
+	}
+}
