f783c7a420cbd260a9f67c4e0f04dca39a456930
Rajat <rajatky3107@gmail.com>
2022-12-18 15:17:54 +0530

    lc 91

---
+++ leetcode/decode-ways-lc-91.go
@@ -0,0 +1,26 @@
+package main
+
+import "strconv"
+
+func main() {
+
+}
+
+func numDecodings(s string) int {
+	dp := make([]int, len(s)+1)
+	dp[0] = 1
+	if s[0] != '0' {
+		dp[1] = 1
+	}
+	for i := 2; i < len(dp); i++ {
+		oneD, _ := strconv.Atoi(s[i-1 : i])
+		twoD, _ := strconv.Atoi(s[i-2 : i])
+		if oneD >= 1 {
+			dp[i] += dp[i-1]
+		}
+		if twoD >= 10 && twoD <= 26 {
+			dp[i] += dp[i-2]
+		}
+	}
+	return dp[len(s)]
+}
