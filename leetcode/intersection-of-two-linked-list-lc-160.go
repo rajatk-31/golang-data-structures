310c1266ba0cd34cbed9dea48462294c49eb2583
Rajat <rajatky3107@gmail.com>
2022-12-22 16:08:09 +0530

    lc 160

---
+++ leetcode/intersection-of-two-linked-list-lc-160.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	if headA == nil || headB == nil {
+		return nil
+	}
+	a := headA
+	b := headB
+	for a != b {
+		fmt.Println(a, b)
+		if a == nil {
+			a = headB
+		} else {
+			a = a.Next
+		}
+		if b == nil {
+			b = headA
+		} else {
+			b = b.Next
+		}
+	}
+	return a
+}
