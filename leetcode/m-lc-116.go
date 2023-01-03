84029aa1c16f331e00f8a3cea33e8a7ed4ca2a0a
Rajat <rajatky3107@gmail.com>
2023-01-03 11:03:20 +0530

    lc 116

---
+++ leetcode/m-lc-116.go
@@ -0,0 +1,42 @@
+package mai
+
+func main() {
+
+}
+
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+	Next  *Node
+}
+
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Left *Node
+ *     Right *Node
+ *     Next *Node
+ * }
+ */
+
+func connect(root *Node) *Node {
+	if root == nil {
+		return root
+	}
+	var pre, cur *Node
+	pre = root
+	for pre.Left != nil {
+		cur = pre
+		for cur != nil {
+			cur.Left.Next = cur.Right
+			if cur.Next != nil {
+				cur.Right.Next = cur.Next.Left
+			}
+			cur = cur.Next
+		}
+		pre = pre.Left
+	}
+	return root
+}
